e152e8ba5a58aff6e76854cadd919332d2b43886
OwayranTorquato <owayran1998@gmail.com>
2024-04-06 19:12:20 -0300

    Bibliotech II

--- JAVA/Logica de Programacao/Palindromo/PalindromeCheck.java
+++ JAVA/Logica de Programacao/Palindromo/PalindromeCheck.java
@@ -11,10 +11,6 @@ public class PalindromeCheck {
             revertedNumber = revertedNumber * 10 + x % 10;
             x /= 10;
         }
-        
-        // Handle cases where the length of the palindrome is odd
-        // For example, if x = 12321, after the loop x = 12 and revertedNumber = 123
-        // We need to check if x == revertedNumber / 10
         return x == revertedNumber || x == revertedNumber / 10;
     }
 }
